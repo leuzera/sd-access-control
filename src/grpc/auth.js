ee2f1edd09acd7e128dd0db27584c4a5805b47fc
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2019-05-01 20:25:35 -0300

    Removing old code for testing

--- src/grpc/auth.js
+++ src/grpc/auth.js
@@ -1,19 +1,9 @@
-let users = [
-  { username: "leonardo", password: "leo123", code: "w2c33caFS3zwS9" },
-  { username: "andreia", password: "senha123", code: "GaG29UJTN273e7F" }
-];
-
 class AuthHandler {
   authPerson(call, callback) {
     let authorized = false;
 
-    for (let u of users) {
-      if (u.code === call.request.token) {
-        authorized = true;
-      }
-    }
+    // TODO: look at database for user
 
-    console.log(authorized);
     return callback(null, { authorized });
   }
 }
