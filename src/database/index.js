db8530e2feeabd5ac298f0f8fec62ed98d213d73
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2019-05-04 18:08:19 -0300

    Checking for error connecting to database

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,14 @@
 const Mongoose = require("mongoose");
+const logger = require("../logger");
 const config = require("../config");
 
-const database = Mongoose.connect(config.mongo_uri, { useNewUrlParser: true });
+const database = Mongoose.connect(
+  config.mongo_uri,
+  { useNewUrlParser: true },
+  err => {
+    if (err) logger.error(err);
+    else logger.debug(`Connected to database with uri ${config.mongo_uri}`);
+  }
+);
 
 module.exports = database;
