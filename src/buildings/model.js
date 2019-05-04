4cb5ecdc8293af88c905869c5d091c1a39f77744
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2019-05-04 20:05:45 -0300

    Renaming variables

--- src/buildings/model.js
+++ src/buildings/model.js
@@ -1,17 +1,17 @@
 const Mongoose = require("mongoose");
 const logger = require("../logger");
 
-const schema = Mongoose.Schema;
+const Schema = Mongoose.Schema;
 
-const buildingSchema = new schema({
+const buildingSchema = new Schema({
   name: { type: String, required: true, trim: true },
   max_capacity: { type: Number, required: true },
   occupancy: { type: Number, required: true },
-  floors: [{ type: ObjectId, ref: "Floors" }]
+  floors: [{ type: Schema.Types.ObjectId, ref: "Floors" }]
 });
 
-const floorsSchema = new schema({
-  _buildingId: { type: ObjectId, ref: "Building" },
+const floorsSchema = new Schema({
+  _buildingId: { type: Schema.Types.ObjectId, ref: "Building" },
   number: { type: Number, required: true },
   capacity: { type: Number, required: true },
   occupancy: { type: Number, required: true, default: 0 }
@@ -62,8 +62,8 @@ buildingSchema.methods.leave = function(floor_id, callback) {
   // decrement floor and building occupancy
 };
 
-const Floor = mongoose.model("Floors", floorsSchema);
-const Building = mongoose.model("Building", buildingSchema);
+const Floor = Mongoose.model("Floors", floorsSchema);
+const Building = Mongoose.model("Building", buildingSchema);
 
 module.exports = {
   Building,
