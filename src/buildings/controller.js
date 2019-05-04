14ef36ae75e6f18fa9c460512d6b66fc1e883389
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2019-05-04 19:59:31 -0300

    Boilerplate for controller and routes

--- src/buildings/controller.js
+++ src/buildings/controller.js
@@ -1,3 +1,29 @@
 const BuildingModel = require("./model");
 const database = require("../database");
 const logger = require("../logger");
+
+function createBuilding(name, capacity) {}
+function recoverBuilding(id) {}
+function recoverAllBuildings() {}
+function deleteBuilding(id) {}
+function updateBuilding(id, params) {}
+
+function createFloor(number, capacity, buildingId) {}
+function recoverFloor(buildingId, number) {}
+function recoverAllFloors(buildingId) {}
+function deleteFloor(buildingId, number) {}
+function updateFloor(buildingId, floor) {}
+
+module.exports = {
+  createBuilding,
+  recoverBuilding,
+  recoverAllBuildings,
+  deleteBuilding,
+  updateBuilding,
+
+  createFloor,
+  recoverFloor,
+  recoverAllFloors,
+  deleteFloor,
+  updateFloor
+};
--- src/buildings/routes.js
+++ src/buildings/routes.js
@@ -1,1 +1,25 @@
 const controller = require("./controller");
+
+module.exports = router => {
+  router
+    .route("/buildings")
+    .get(controller.recoverAllBuildings)
+    .post(controller.createBuilding);
+
+  router
+    .route("/building/:id")
+    .get(controller.recoverBuilding)
+    .put(controller.updateBuilding)
+    .delete(controller.deleteBuilding);
+
+  router
+    .route("/building/:id/floors")
+    .get(controller.recoverAllFloors)
+    .post(controller.createFloor);
+
+  router
+    .route("/building/:id/floor/:number")
+    .get(controller.recoverFloor)
+    .put(controller.updateFloor)
+    .delete(controller.deleteFloor);
+};
