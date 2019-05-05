1e8663fc2660da4ea146d3fa561c852df4d9ad9a
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2019-05-05 16:00:13 -0300

    Implementing buildings functions

--- src/buildings/controller.js
+++ src/buildings/controller.js
@@ -2,17 +2,53 @@ const BuildingModel = require("./model");
 const database = require("../database");
 const logger = require("../logger");
 
-function createBuilding(name, capacity) {}
-function recoverBuilding(id) {}
-function recoverAllBuildings() {}
-function deleteBuilding(id) {}
-function updateBuilding(id, params) {}
+function createBuilding(name, max_capacity, callback) {
+  database.then(
+    () => {
+      const building = new BuildingModel({ name, max_capacity });
 
-function createFloor(number, capacity, buildingId) {}
-function recoverFloor(buildingId, number) {}
-function recoverAllFloors(buildingId) {}
-function deleteFloor(buildingId, number) {}
-function updateFloor(buildingId, floor) {}
+      building.save((err, building) => {
+        if (err) {
+          logger.error(err);
+          callback(err);
+        } else {
+          logger.info("Building added.");
+          logger.debug(building);
+          callback(null, building);
+        }
+      });
+    },
+    err => {
+      logger.error("Error conecting to database.");
+      logger.error(err);
+      callback(err);
+    }
+  );
+}
+
+function recoverAllBuildings(callback) {
+  database.then(() => {
+    BuildingModel.find({}, (err, builds) => {
+      if (err) {
+        logger.error("Error retrieving buildings.");
+        logger.error(err);
+        callback(err);
+      } else {
+        callback(null, builds);
+      }
+    });
+  });
+}
+
+function recoverBuilding(id, callback) {}
+function deleteBuilding(id, callback) {}
+function updateBuilding(id, params, callback) {}
+
+function createFloor(number, capacity, buildingId, callback) {}
+function recoverFloor(buildingId, number, callback) {}
+function recoverAllFloors(buildingId, callback) {}
+function deleteFloor(buildingId, number, callback) {}
+function updateFloor(buildingId, floor, callback) {}
 
 module.exports = {
   createBuilding,
--- src/buildings/routes.js
+++ src/buildings/routes.js
@@ -1,10 +1,30 @@
 const controller = require("./controller");
+const logger = require("../logger");
 
 module.exports = router => {
   router
     .route("/buildings")
-    .get(controller.recoverAllBuildings)
-    .post(controller.createBuilding);
+    .get((_req, res) => {
+      controller.recoverAllBuildings((err, buildings) => {
+        if (!err) {
+          res.status(200).send({ status: 200, buildings: buildings });
+        } else {
+          res.status(500).send({ status: 500 });
+        }
+      });
+    })
+    .post((req, res) => {
+      const { name, capacity } = req.body;
+      controller.createBuilding(name, capacity, (err, building) => {
+        logger.debug(building);
+
+        if (!err) {
+          res.status(201).send({ status: 201, building: building });
+        } else {
+          res.status(500).send({ status: 500 });
+        }
+      });
+    });
 
   router
     .route("/building/:id")
