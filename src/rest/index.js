980e22b080b751fe65162263e5c68d56d021fbaf
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2019-06-02 20:20:41 -0300

    CORS for all?

--- src/rest/index.js
+++ src/rest/index.js
@@ -9,18 +9,6 @@ const cors = require("cors");
 const RestServer = port => {
   const app = express();
 
-  let whitelist = ["https://sd-access-control.netlify.com"];
-
-  let corsOptions = {
-    origin: function(origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    }
-  };
-
   app.use(bodyParser.json());
   app.use(
     bodyParser.urlencoded({
@@ -29,7 +17,8 @@ const RestServer = port => {
   );
 
   app.use(helmet());
-  app.use(cors(corsOptions));
+  app.use(cors());
+  app.options("*", cors());
   app.use(morgan("combined"));
   app.use("/api/v1", routes);
 
