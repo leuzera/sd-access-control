4b015908f9a4b7d91082e8cbccd6d72a824318c0
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2019-06-02 20:03:09 -0300

    CORS

--- src/rest/routes.js
+++ src/rest/routes.js
@@ -3,6 +3,9 @@ const buildings = require("../buildings");
 const groups = require("../group");
 
 module.exports = router => {
+  router.route("/").get((_req, res) => {
+    res.status(418).send({ message: "I am a Teapot." });
+  });
   users(router);
   buildings(router);
   groups(router);
