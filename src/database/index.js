442a6dc86989bf1be88f13a92947070ca03d850d
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2019-06-09 09:56:03 -0300

    Better database connection error

--- src/database/index.js
+++ src/database/index.js
@@ -2,13 +2,9 @@ const Mongoose = require("mongoose");
 const logger = require("../logger");
 const config = require("../config");
 
-const database = Mongoose.connect(
-  config.mongo_uri,
-  { useNewUrlParser: true },
-  err => {
-    if (err) logger.error(err);
-    else logger.debug(`Connected to database with uri ${config.mongo_uri}`);
-  }
-);
+const database = Mongoose.connect(config.mongo_uri, { useNewUrlParser: true }, err => {
+  if (err) logger.error("Could not connect to database.");
+  else logger.debug(`Connected to database with uri ${config.mongo_uri}`);
+});
 
 module.exports = database;
