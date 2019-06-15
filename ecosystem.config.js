1cddbf13efa2f457402a425e7440c221f3eb9a5f
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2019-06-15 13:05:28 -0300

    changing path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "API",
-      script: "./frontend/app.js",
+      script: "./backend/app.js",
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
